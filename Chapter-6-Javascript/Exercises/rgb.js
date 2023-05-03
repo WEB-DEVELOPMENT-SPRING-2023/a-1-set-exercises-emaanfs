c91319ba6e4b9fb5c7e8e62c3ad93ee8bb5bea55
emaan fatima <emaanfs@hotmail.com>
2023-05-03 22:09:31 +0400

    Web Development

--- Chapter-6-Javascript/Exercises/rgb.js
+++ Chapter-6-Javascript/Exercises/rgb.js
@@ -9,6 +9,8 @@ var b= Math.floor((Math.random())*256);
 //Get element's purpose is to return an elements'/'s object which shows whose id matchs a described string//
 document.getElementById("colorcode").innerHTML="( "+r+" , "+g+" , "+b+" )";
 
+
+//index will provide direct access to the data or other function //
 var index=Math.floor((Math.random())*3);
 
 document.getElementById(index).style.backgroundColor="rgb("+r+","+g+","+b+")";
@@ -21,6 +23,8 @@ var g1= Math.floor((Math.random())*256);
 var b1= Math.floor((Math.random())*256);
 document.getElementById(arr[0]).style.backgroundColor="rgb("+r1+","+g1+","+b1+")";
 
+
+//MAth Random is used for selecting random numbers or data from larger numbers or data group//
 var r2= Math.floor((Math.random())*256);
 var g2= Math.floor((Math.random())*256);
 var b2= Math.floor((Math.random())*256);
@@ -28,6 +32,7 @@ document.getElementById(arr[1]).style.backgroundColor="rgb("+r2+","+g2+","+b2+")
 
 
 function guess(){
+    //inner html is used to get all the internal elements/content which are in the html file//
     if(document.activeElement.id==index){
         document.getElementById("colorcode").innerHTML="CORRECT !!";
         document.getElementById("colorcode").style.color="darkgreen";
@@ -36,14 +41,15 @@ function guess(){
         document.getElementById("colorcode").innerHTML="WRONG !!";
         document.getElementById("colorcode").style.color="red";
     }
-
-    document.getElementsByClasscode("btn")[0].removeAttribute("onclick");
-    document.getElementsByClasscode("btn")[1].removeAttribute("onclick");
-    document.getElementsByClasscode("btn")[2].removeAttribute("onclick");
+ //Element by class name is used to get/return all the elements which are in 'btn' class()//
+    document.getElementsByClassName("btn")[0].removeAttribute("onclick");
+    document.getElementsByClassName("btn")[1].removeAttribute("onclick");
+    document.getElementsByClassName("btn")[2].removeAttribute("onclick");
 
 
 }
 
+//restart function is set to the button created in html, so when clicked the game would restart//
 function restart(){
     window.location.reload();
 }
