752cb415e56fa795c93aa65954b85c9f0cc0c219
emaan fatima <emaanfs@hotmail.com>
2023-05-03 22:46:48 +0400

    web development

--- Chapter-6-Javascript/Exercises/chp6.js
+++ Chapter-6-Javascript/Exercises/chp6.js
@@ -1,10 +1,15 @@
 
+//function for gas calculator has been declared to sum up a section of code.//
 function gascalculator(){
+
+    //document has been used to allow script to change and format  //
     var gasperlitre=document.getElementById("Gaslitre").value;
     var gaspurchased=document.getElementById("GasPurchased").value;
 
+    //variable has been declared to calculate the gas//
     var TotalAmount=gasperlitre*gaspurchased;
 
+    //function created to fetch the html element by its id or class to act upon it//
     document.getElementById("TotalCost").innerHTML="Total Amount to be paid $" + TotalAmount;
     
 }
